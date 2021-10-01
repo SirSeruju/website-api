e9ea4d5387294d79f3c106b8d8d3be6c85229122
Seruju <sir.seruju@yandex.ru>
2021-10-01 12:19:12 +0300

    Add POST request for posts.

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,18 @@
 module Main where
 
 import Web.Scotty
-import Data.Aeson (object, (.=), Value (Null))
+import Data.Aeson (object, (.=), Value (Null, Number), fromJSON, toJSON, Result(..))
 import Control.Monad.IO.Class (liftIO)
 import Network.HTTP.Types
 import System.Directory
 import Network.Wai.Middleware.HttpAuth
+import Network.Wai
 import Network.HTTP.Types.Status (noContent204)
+import Data.ByteString
+import GHC.Int
+import Data.HashMap.Strict
+import Data.Text
+import Control.Exception
 
 import Auth
 import Model
@@ -18,20 +24,41 @@ main = do
   scotty 3000 $ do
     get  "/api/post"      $ getPostsA
     get  "/api/post/:pid" $ param "pid" >>= getPostByIdA
+    post "/api/post"      $ postPostA
     --middleware $ basicAuth (verify) authSettings
     --Sc.post "/api/content"      $ postContent conn
 
 getPostsA :: ActionM ()
 getPostsA = do
-  posts <- liftIO getPosts
+  posts <- liftAndCatchIO selectPosts
   json $ object ["posts" .= posts]
 
 getPostByIdA :: Integer -> ActionM ()
 getPostByIdA index = do
-  post <- liftIO $ getPostById index
+  post <- liftAndCatchIO $ selectPostById index
   maybe noContentA json post
 
-noContentA :: ActionM ()
-noContentA = do
-  status noContent204
-  json Null
+postPostA :: ActionM ()
+postPostA = do
+  userId <- loginA
+  post <- (jsonData :: ActionM (HashMap Text Value)) >>= (\h -> return $ insert "user" (Number . fromIntegral $ userId) h)
+  liftAndCatchIO ((catchResult . fromJSON . toJSON $ post) >>= insertPost) >>= json
+  where
+    catchResult :: Result a -> IO a
+    catchResult (Error err) = error err
+    catchResult (Success a) = return a
+  
+loginA :: ActionM Int64
+loginA = do
+  req <- request
+  case (Prelude.lookup hAuthorization $ requestHeaders req) >>= extractBasicAuth of
+    Just (username, password) -> do
+      username <- liftAndCatchIO . validateUser $ User username password
+      maybe unauthorizedA return username
+
+noContentA :: ActionM a
+noContentA = raiseStatus noContent204 "No content."
+
+unauthorizedA :: ActionM a
+unauthorizedA = raiseStatus unauthorized401 "Basic Authorization needed."
+
--- src/Model.hs
+++ src/Model.hs
@@ -12,15 +12,24 @@
 {-# LANGUAGE DataKinds                  #-}
 
 module Model
-  ( doMigration, getPosts, getPostById)
+  ( doMigration
+  , selectPosts
+  , selectPostById
+  , insertPost
+  , validateUser
+  , Post(..)
+  , User(..) )
 where
 
 import Database.Persist
 import Database.Persist.TH
 import Database.Persist.Sqlite
+import Database.Persist.Class
+import Crypto.BCrypt
 import Data.ByteString
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Logger   (runStderrLoggingT)
+import GHC.Int
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Post json
@@ -30,9 +39,9 @@ Post json
   UniqueTitle title
   deriving Show
 User
-  user String
+  username ByteString
   password ByteString
-  UniqueUser user
+  UniqueUser username
   deriving Show
 |]
 
@@ -45,12 +54,21 @@ runDb query = do
 doMigration :: IO ()
 doMigration = runDb (runMigration migrateAll)
 
-getPosts :: IO [Entity Post]
-getPosts = runDb $ selectList ([] :: [Filter Post]) []
+selectPosts :: IO [Entity Post]
+selectPosts = runDb $ selectList ([] :: [Filter Post]) []
 
-getPostById :: Integer -> IO (Maybe (Entity Post))
-getPostById index = runDb $ do
-  posts <- selectList [PostId ==. (toSqlKey . fromIntegral $ index)] []
-  case posts of
-    (p:_) -> return . Just $ p 
-    []    -> return Nothing
+selectPostById :: Integer -> IO (Maybe (Entity Post))
+selectPostById index = runDb $ selectFirst [PostId ==. (toSqlKey . fromIntegral $ index)] []
+
+validateUser :: User -> IO (Maybe Int64)
+validateUser (User username password) = do
+  user <- runDb $ selectFirst [UserUsername ==. username] []
+  return $ (entityVal <$> user) >>= validateP >> (fromSqlKey . entityKey <$> user)
+  where
+    validateP user@(User _ passHash) =
+      if validatePassword passHash password
+      then Just $ username
+      else Nothing
+
+insertPost :: Post -> IO (Key Post)
+insertPost = runDb . insert
