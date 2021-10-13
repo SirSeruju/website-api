1d6caccd5bbd1b666550001bafc21d2612e2a121
Seruju <sir.seruju@yandex.ru>
2021-10-13 13:13:15 +0300

    Remove unused for pedantic build.

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,7 @@
 module Main where
 
 import Web.Scotty
-import Data.Aeson (object, (.=), Value (Null, Number), fromJSON, toJSON, Result(..))
-import Control.Monad.IO.Class (liftIO)
-import Network.HTTP.Types
-import System.Directory
-import Network.Wai.Middleware.HttpAuth
-import Network.Wai
-import Network.HTTP.Types.Status (noContent204)
-import Data.ByteString
-import GHC.Int
-import Data.HashMap.Strict
-import Data.Text
-import Control.Exception
 
-import Auth
 import Model
 import Actions (getPostsA, getPostByIdA, postPostA)
 
