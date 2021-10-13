1fcf0859684c26f61aa2ef760d5c078a2acbcf82
Seruju <sir.seruju@yandex.ru>
2021-10-13 16:11:42 +0300

    Add config support.

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,35 @@
 module Main where
 
 import Web.Scotty
+  ( scottyOpts
+  , Options (Options, verbose, settings)
+  , param
+  , get
+  , post )
+import Network.Wai.Handler.Warp (defaultSettings, setPort)
+import Data.String (fromString)
 
 import Model
 import Actions (getPostsA, getPostByIdA, postPostA)
+import qualified Config (port, apiPrefix, verbose)
+
+options :: IO Options
+options = do
+  verb <- Config.verbose
+  port <- Config.port
+  return $ Options
+    { verbose = verb
+    , settings = setPort port defaultSettings
+    }
 
 main :: IO ()
 main = do
   doMigration
-  scotty 3000 $ do
-    get  "/api/post"      $ getPostsA
-    get  "/api/post/:pid" $ param "pid" >>= getPostByIdA
-    post "/api/post"      $ postPostA
+  prefix <- Config.apiPrefix
+  opts <- options
+  scottyOpts opts $ do
+    get  (fromString (prefix ++ "post"))      $ getPostsA
+    get  (fromString (prefix ++ "post/:pid")) $ param "pid" >>= getPostByIdA
+    post (fromString (prefix ++ "post"))      $ postPostA
     --middleware $ basicAuth (verify) authSettings
     --Sc.post "/api/content"      $ postContent conn
---
+++ src/Config.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Config where
+
+import Data.Configurator
+  ( load
+  , Worth(Required)
+  , lookupDefault )
+import Data.Configurator.Types (Config)
+
+config :: IO Config
+config = load [Required "config"]
+
+port :: IO Int
+port = config >>= (\c -> lookupDefault 8080 c "server.port")
+
+apiPrefix :: IO String
+apiPrefix = config >>= (\c -> lookupDefault "/api/" c "server.api_prefix")
+
+dbFileName :: IO String
+dbFileName = config >>= (\c -> lookupDefault "Database.db" c "database.filename")
+
+verbose :: IO Int
+verbose = config >>= (\c -> lookupDefault 0 c "server.verbose")
